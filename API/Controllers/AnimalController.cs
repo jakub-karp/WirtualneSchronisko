29e4a8c9f570ec907850e194a5b59d8c48a1bedb
Bartosz Grabowski <bartek.grabowski.280@gmail.com>
2023-05-24 12:11:26 +0200

    +poprawa endpointow i rozlozenie ich na dwie czesci

---
+++ API/Controllers/AnimalController.cs
@@ -0,0 +1,69 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalController : ControllerBase
+    {
+        private readonly IAsyncDocumentSession session;
+        private readonly string animalIdString = "animals/";
+
+        public AnimalController(IAsyncDocumentSession Session)
+        {
+            session = Session;
+        }
+
+        // GET: api/<AnimalController>
+        [HttpGet]
+        public async Task<List<Animal>> Get()
+        {
+            var animals = await session
+                .Query<Animal>()
+                .ToListAsync();
+            return animals;
+        }
+
+        // GET api/<AnimalController>/5
+        [HttpGet("{id}")]
+        public async Task<Animal> Get(string id)
+        {
+            id = animalIdString + id;
+            var animal = await session.LoadAsync<Animal>(id);
+            return animal;
+        }
+
+        // POST api/<ShelterController>
+        [HttpPost]
+        public async Task Post(string id, [FromBody] Animal animal)
+        {
+            id = animalIdString + id;
+            var shelter = await session.LoadAsync<Shelter>(id);
+            shelter.Animals.Add(animal);
+            await session.SaveChangesAsync();
+        }
+
+        // PUT api/<AnimalController>/5
+        [HttpPut("{id}")]
+        public async Task Put(string id, [FromBody] Animal animal)
+        {
+            await session.StoreAsync(animal);
+            await session.SaveChangesAsync();
+        }
+
+        // DELETE api/<ShelterController>/5
+        [HttpDelete("{id}")]
+        public async Task DeleteAnimal(string id)
+        {
+            id = animalIdString + id;
+            var animal = await session.LoadAsync<Animal>(id);
+            session.Delete(animal);
+            await session.SaveChangesAsync();
+        }
+    }
+}
--- API/Controllers/ShelterController.cs
+++ API/Controllers/ShelterController.cs
@@ -42,55 +42,27 @@ namespace API.Controllers
 
         // POST api/<ShelterController>
         [HttpPost]
-        public async Task PostShelter([FromBody] Shelter newShelter)
+        public async Task Post([FromBody] Shelter newShelter)
         {
             await session.StoreAsync(newShelter);
             await session.SaveChangesAsync();
         }
 
-        // POST api/<ShelterController>
-        [HttpPost]
-        public async Task PostAnimal(string id, [FromBody] Animal animal)
-        {
-            id = shelterIdString + id;
-            var shelter = await session.LoadAsync<Shelter>(id);
-            shelter.Animals.Add(animal);
-            await session.SaveChangesAsync();
-        }
-
         // PUT api/<ShelterController>/5
         [HttpPut("{id}")]
-        public async Task PutShelter(string id, [FromBody] Shelter shelter)
+        public async Task Put(string id, [FromBody] Shelter shelter)
         {
             await session.StoreAsync(shelter);
             await session.SaveChangesAsync();
         }
 
-        // PUT api/<ShelterController>/5
-        [HttpPut("{id}")]
-        public async Task PutAnimal(string id, [FromBody] Shelter animal)
-        {
-            await session.StoreAsync(animal);
-            await session.SaveChangesAsync();
-        }
-
         // DELETE api/<ShelterController>/5
         [HttpDelete("{id}")]
-        public async Task DeleteShelter(string id)
+        public async Task Delete(string id)
         {
             id = shelterIdString + id;
             var shelter = await session.LoadAsync<Shelter>(id);
             session.Delete(shelter);
-            await session.SaveChangesAsync();
-        }
-
-        // DELETE api/<ShelterController>/5
-        [HttpDelete("{id}")]
-        public async Task DeleteAnimal(string id)
-        {
-            id = shelterIdString + id;
-            var animal = await session.LoadAsync<Animal>(id);
-            session.Delete(animal);
             await session.SaveChangesAsync();
         }
     }
